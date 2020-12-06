d923e42abb420082bb232569cb2233f8c3f72e3b
Lakshith Dabas <ld2720@ic.ac.uk>
2020-12-06 17:27:01 +0000

    initial commit

---
+++ preprocess_btc.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocesing import MinMaxScaler
+
+plt.style.use("ggplot")
+
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout
+
+print("Hello")
